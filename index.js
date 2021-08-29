ac6e597d8c72dd2eb0f5b0fb63d3229dbc95c906
Nathan Irvin <65375604+irv0735@users.noreply.github.com>
2021-08-28 20:01:45 -0600

    attempted to use a promise to call the class constructor - did not work

--- index.js
+++ index.js
@@ -3,7 +3,16 @@ const Engineer = require('./lib/engineer')
 const Intern = require('./lib/intern')
 
 
-const john = new Engineer();
+const newManager = new Promise((resolve, reject) => {
+  const John = new Manager()
+  if (!John.officeNumber) {
+    reject("entry not complete")
+  } else {
+    resolve("good to go, data is here!")
+  }
+});
 
-
+newManager
+  .then((data) => console.log(data))
+  .catch(err => console.log(`Promise rejected: Error: Coding stopped - ${err.message}`));
 
