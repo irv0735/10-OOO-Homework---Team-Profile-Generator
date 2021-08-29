fdcf465cffd11fffa01039473d95a2b5cc3e6c88
Nathan Irvin <65375604+irv0735@users.noreply.github.com>
2021-08-28 18:42:59 -0600

    started creating the employee.js and test file, but am in way over my head. stepping back to review

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,17 @@
+const Employee = require("../lib/employee");
+
+describe("Employee", () => {
+  describe("Initialization", () => {
+    it("should return an object containing name, id, email and role properties when called with the 'new' keyword", () => {
+      const obj = new Employee();
+
+      expect("name" in obj).toBe(true);
+      expect("id" in obj).toBe(true);
+      expect("email" in obj).toBe(true);
+      expect("role" in obj).toBe(true);
+    });
+  });
+
+  describe()
+
+});
--- index.js
+++ index.js
@@ -1,1 +1,99 @@
-const inquirer = require("inquirer");
+const Employee = require('./lib/employee')
+const Manager = require('./lib/manager')
+const Engineer = require('./lib/engineer')
+const Intern = require('./lib/intern')
+
+
+// const promptInitial = () => {
+//   return inquirer.prompt([
+//     {
+//       type: 'input',
+//       name: 'name',
+//       message: "Team Manager's Name: ",
+//     },
+//     {
+//       type: 'input',
+//       name: 'id',
+//       message: "Employee ID: ",
+//     },
+//     {
+//       type: 'input',
+//       name: 'email',
+//       message: "Email Address: ",
+//     },
+//     {
+//       type: 'input',
+//       name: 'office',
+//       message: "Office Number: ",
+//     },
+//     {
+//       type: 'list', 
+//       name: 'nextSelection',
+//       message: "Would you like to add another employee or are you finished?",
+//       choices: ['Add Engineer', 'Add Intern', 'Finished Building Team'],
+//     }
+//   ]);
+// };
+
+// const promptEngineer = () => {
+//   return inquirer.prompt([
+//     {
+//       type: 'input',
+//       name: 'name',
+//       message: "Engineer's Name: ",
+//     },
+//     {
+//       type: 'input',
+//       name: 'id',
+//       message: "Employee ID: ",
+//     },
+//     {
+//       type: 'input',
+//       name: 'email',
+//       message: "Email Address: ",
+//     },
+//     {
+//       type: 'input',
+//       name: 'gitHub',
+//       message: "GitHub Username: ",
+//     },
+//     {
+//       type: 'list', 
+//       name: 'nextSelection',
+//       message: "Would you like to add another employee or are you finished?",
+//       choices: ['Add Engineer', 'Add Intern', 'Finished Building Team'],
+//     }
+//   ]);
+// };
+
+// const promptIntern = () => {
+//   return inquirer.prompt([
+//     {
+//       type: 'input',
+//       name: 'name',
+//       message: "Engineer's Name: ",
+//     },
+//     {
+//       type: 'input',
+//       name: 'id',
+//       message: "Employee ID: ",
+//     },
+//     {
+//       type: 'input',
+//       name: 'email',
+//       message: "Email Address: ",
+//     },
+//     {
+//       type: 'input',
+//       name: 'school',
+//       message: "School: ",
+//     },
+//     {
+//       type: 'list', 
+//       name: 'nextSelection',
+//       message: "Would you like to add another employee or are you finished?",
+//       choices: ['Add Engineer', 'Add Intern', 'Finished Building Team'],
+//     }
+//   ]);
+// };
+
