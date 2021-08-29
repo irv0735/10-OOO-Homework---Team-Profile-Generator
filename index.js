0f941a5b006f0862e231fec0d5915b902410b717
Nathan Irvin <65375604+irv0735@users.noreply.github.com>
2021-08-28 20:27:35 -0600

    still trying to find ways to walk through the inquirer prompts and wait to move on to the next section of code

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
+const Employee = require('./lib/employee')
 const Manager = require('./lib/manager')
 const Engineer = require('./lib/engineer')
 const Intern = require('./lib/intern')
 
 
-const newManager = new Promise((resolve, reject) => {
-  const John = new Manager()
-  if (!John.officeNumber) {
-    reject("entry not complete")
-  } else {
-    resolve("good to go, data is here!")
-  }
-});
-
-newManager
-  .then((data) => console.log(data))
-  .catch(err => console.log(`Promise rejected: Error: Coding stopped - ${err.message}`));
+const John = new Manager();
 
