68eb3cd485d42cdf7f90fba24b2750fc2d08769f
Nathan Irvin <65375604+irv0735@users.noreply.github.com>
2021-08-28 22:38:09 -0600

    added an array to store team members

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
+const inquirer = require("inquirer")
 const Employee = require('./lib/employee')
 const Manager = require('./lib/manager')
 const Engineer = require('./lib/engineer')
 const Intern = require('./lib/intern')
 
+let teamMembers = [];
 
 function init () {
   const newEmployee = new Manager();
-  let newName = newEmployee.getName();
-  let newID = newEmployee.getID();
-  let newEmail = newEmployee.getEmail();
+  newEmployee.getName();
+  newEmployee.getID();
+  newEmployee.getEmail();
+  newEmployee.getOfficeNumber();
+  newEmployee.getRole();
+  teamMembers.push(newEmployee);
+  console.log(teamMembers);
 }
 
 init()
